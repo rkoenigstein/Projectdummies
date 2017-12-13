934bed83b197a501d00f62421b8643776cf0f175
Robert Königstein <robert.koenigstein@t-online.de>
2017-12-13 10:25:26 +0100

    Basic C++ MPI program added. Prints host of each process. Makefile, gitignore and README added.

---
+++ C++_MPI_project/helloworld.cpp
@@ -0,0 +1,27 @@
+#define _BSD_SOURCE
+#include <stdio.h>
+#include <mpi.h>
+#include <unistd.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	int rank;
+	int commsize;
+	char hostname[80];
+	FILE *file;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+
+	file = popen("cat /proc/sys/kernel/hostname", "r");
+	fscanf(file, "%s", hostname);
+	fclose(file);
+
+	std::cout << "Rank: " << rank << ", Commsize: " << commsize << ", host: " << hostname << std::endl;
+
+	MPI_Finalize();
+
+	return 0;
+}
